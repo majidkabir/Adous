56309ff99739a6083f63b595af3573eb6ebeb5c1
Majid Ghafouri <majid.ghafouri@maersk.com>
2025-10-02 09:45:58 +0200

    Initializing the project

---
+++ src/main/java/app/majid/Adous/AdousApplication.java
@@ -0,0 +1,13 @@
+package app.majid.Adous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/majid/Adous/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package app.majid.Adous;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
