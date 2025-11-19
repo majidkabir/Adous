976fb4ff157818fd5b33cc6388acc9b0ddb27c65
Majid Ghafouri <majidkabir@gmail.com>
2025-11-19 22:07:35 +0100

    Fixing default schema in SQL equal (#6)

--- src/test/java/app/majid/adous/synchronizer/service/SqlEquivalenceCheckerServiceTest.java
+++ src/test/java/app/majid/adous/synchronizer/service/SqlEquivalenceCheckerServiceTest.java
@@ -9,8 +9,8 @@ class SqlEquivalenceCheckerServiceTest {
 
     @Test
     void viewsWithDifferentQuotingAndWhitespaceShouldBeEquivalent() throws Exception {
-        String sqlA = "SET ANSI_NULLS OFF; GO SET QUOTED_IDENTIFIER OFF; GO  CREATE   VIEW [BI].[V_SKU]  AS SELECT *  FROM [SKU] WITH (NOLOCK)  GO";
-        String sqlB = "SET ANSI_NULLS OFF; GO SET QUOTED_IDENTIFIER OFF; GO  CREATE VIEW BI.V_SKU AS      SELECT *     FROM [SKU] WITH (NOLOCK)    GO";
+        String sqlA = "CREATE   VIEW [BI].[V_SKU]  AS SELECT *  FROM [SKU] WITH (NOLOCK)  GO";
+        String sqlB = "CREATE VIEW BI.V_SKU AS      SELECT *     FROM [SKU] WITH (NOLOCK)    GO";
 
         SqlEquivalenceCheckerService service = new SqlEquivalenceCheckerService();
         // Inject default schema expected by service (property normally provided by Spring)
@@ -20,4 +20,20 @@ class SqlEquivalenceCheckerServiceTest {
 
         assertTrue(service.equals(sqlA, sqlB), "Expected the two CREATE VIEW statements to be normalized as equivalent");
     }
+
+    @Test
+    void viewsWithDefaultSchemaInSourceTableShouldBeEquivalent() throws Exception {
+        String sqlA = "CREATE    VIEW [BI].[V_ChannelInv] AS  \nSELECT *\nFROM dbo.V_ChannelInv with (nolock)\nGO";
+
+        String sqlB = "/***/\nCREATE   VIEW [BI].[V_ChannelInv]\nAS\nSELECT * FROM V_ChannelInv WITH (NOLOCK)\nGO";
+
+        SqlEquivalenceCheckerService service = new SqlEquivalenceCheckerService();
+        // Inject default schema expected by service (property normally provided by Spring)
+        Field f = SqlEquivalenceCheckerService.class.getDeclaredField("defaultSchema");
+        f.setAccessible(true);
+        f.set(service, "dbo");
+
+        assertTrue(service.equals(sqlA, sqlB), "Expected views to be equivalent: dbo.V_ChannelInv vs V_ChannelInv (dbo is default schema)");
+    }
 }
+
